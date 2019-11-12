aef7213504b288ef60cfbb98b04b6d744bca38d6
Jeff Johnson <mightymosjeff@gmail.com>
2019-11-12 10:27:21 -0600

    Updated server.js

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ const app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+};
+
+app.use(allowCrossDomain);
+
 // Programs
 app.get('/api/programs', (req, res) => {
     db.Program.findAll()
